6a65cb17c3948483c8fe7acd6c06dc509cbd2dac
Boris Parusnikov <2669997@gmail.com>
2021-05-17 20:01:02 +0300

    takes money test added

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -80,5 +80,20 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(game_path(game))
       expect(flash.empty?).to be_truthy # удачный ответ не заполняет flash
     end
+
+    it 'takes money' do
+      game_w_questions.update_attribute(:current_level, 4)
+      put :take_money, params: { id: game_w_questions.id }
+      game = assigns(:game)
+      expect(game.finished?).to be_truthy
+      expect(game.status).to eq(:money)
+      expect(game.prize).to eq(500)
+
+      user.reload
+      expect(user.balance).to eq(500)
+
+      expect(response).to redirect_to(user_path(user))
+      expect(flash[:warning]).to be
+    end
   end
 end
