2abf039c8bdd2aeeeef1efee176b4895bdfe459b
knmrftw <2669997@gmail.com>
2021-05-20 20:53:39 +0300

    show.html.erb_spec.rb tests started

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,20 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
+  let(:user) { FactoryBot.create :user }
+
   before(:each) do
-    assign(:user,
-      FactoryBot.build_stubbed(:user, name: 'Борис', balance: 5000))
+    assign(:user, user)
+    assign(:games, [
+      FactoryBot.build_stubbed(:game, id: 1, current_level: 3, prize: 300)
+    ])
 
     render
+  end
+
+  context 'user == current_user' do
+    before(:each) do 
+      sign_in user
+      render
     end
 
-  it 'renders player name' do
-    
+    it 'renders users name' do
+      expect(rendered).to match user.name
+    end
+
+    it 'renders psswd chng link' do
+      expect(rendered).to match edit_user_registration_path(user)
+    end
+
+    it 'render games' do
+      expect(rendered).to match '3'
+      expect(rendered).to match '300 ₽'
+    end
   end
 
-  it 'renders psswd chng button for current_user' do
-  end
+  context 'user != current_user' do
+    it 'renders users name' do
+      expect(rendered).to match user.name
+    end
 
-  it 'renders game parts' do
+    it 'not renders psswd chng link' do
+      expect(rendered).not_to match edit_user_registration_path(user)
+    end
+
+    it 'render games' do
+      expect(rendered).to match '3'
+      expect(rendered).to match '300 ₽'
+    end
   end
 end
