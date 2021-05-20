c9d6afc77623b67b44b5e678b981483114656a7c
knmrftw <2669997@gmail.com>
2021-05-20 23:04:28 +0300

    User viisits other users profile

---
+++ spec/features/user_visits_other_users_profile_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature 'USER visits other users profile', type: :feature do
+  let(:profile_owner) { FactoryBot.create(:user) }
+  let!(:games) do
+    [
+      FactoryBot.create(:game,
+      user: profile_owner,
+      current_level: 3,
+      prize: 300,
+      created_at: "2012-02-27 10:00:00"
+    ),
+    FactoryBot.create(:game,
+      user: profile_owner,
+      current_level: 5,
+      prize: 500,
+      created_at: "2012-02-27 11:00:00",
+      finished_at: "2012-02-27 11:30:00"
+    )
+    ]
+  end
+
+  scenario 'success' do
+    # binding.pry
+    visit user_path(profile_owner)
+
+    expect(page).to have_content profile_owner.name
+    expect(page).not_to have_content edit_user_registration_path(profile_owner)
+
+    expect(page).to have_content '3'
+    expect(page).to have_content '300 ₽'
+    expect(page).to have_content 'в процессе'
+    expect(page).to have_content '27 февр., 10:00'
+
+    expect(page).to have_content '5'
+    expect(page).to have_content '500 ₽'
+    expect(page).to have_content 'деньги'
+    expect(page).to have_content '27 февр., 11:00'
+  end
+end
